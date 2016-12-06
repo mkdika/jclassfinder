fd8696f6b11b438fd3fca90303e98bc92ea7ede8
maikel <maikel@maikel-mint>
2016-12-06 09:37:04 +0700

    fix title

--- src/main/java/com/mkdika/jclassfinder/config/AppConfig.java
+++ src/main/java/com/mkdika/jclassfinder/config/AppConfig.java
@@ -7,7 +7,7 @@ package com.mkdika.jclassfinder.config;
  */
 public class AppConfig {
     
-    public static final String APP_TITLE = "Java Class & JAR Finder";
+    public static final String APP_TITLE = "Java JAR Class Finder";
     public static final String APP_SHORT_TITLE = "JClassFinder";
     public static final String APP_VERSION = "1.0.0";
     public static final String[] CONTRIBUTOR = {"Maikel Chandika (mkdika@gmail.com)"};
